3ed27ad0574def453a4d72e6621c19c31efd7e0a
AlexRickert <ARSounders@me.com>
2015-05-23 14:15:04 -0700

    cup class
    I made the cup class in the gameplay directory

---
+++ gameplay/cup.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Alex on 5/23/2015.
+ */
+public class cup {
+
+    int size;
+    boolean color;
+
+    public cup(int size, boolean color) {
+        this.size = size;
+        this.color = color;
+    }
+
+    public boolean getColor() {
+        return color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
