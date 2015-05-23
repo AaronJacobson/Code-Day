d9446fe8d2ebfd0cac08c436abfc32cc584b9256
Ian Flaherty <ianrocket@gmail.com>
2015-05-23 16:00:15 -0700

    I DONT KNOW WHAT IM DOING!   <-- that is a bad commit msg #DoNtDoThAt

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-var draw = function(){
+var url,
+  protocal,
+  ws;
 
+
+var draw = function(){
+  var canvas = new Canvas();
 }
 
-var MY_VARS = function(){
-    var globals = {
-        URL : "http://someUrl",
-        protocal : "blah";
-    }
-    return{ getUrl : function(s){
-        return globals[s];
-      }
-    }
-}();
+function WebSocketTest()
+{
+  if("webSocket" in window){
+    ws = new webSocket(url, protocal);
+    ws.onopen = onSocketConnected();
+    ws.onclose = onSocketDisconnect();
+    ws.onmessage = onSocketMessage();
+    ws.onerror = onSocketError();
 
+       // Socket message
+      function onSocketMessage(message) {
+          console.log('Message: ' + message.data);
+      };
 
-var webSocket = function(){
- var socket = new webSocket(MY_VARS.getUrl('URL'), MY_VARS.getUrl('protocal');
+      // Socket error
+      function onSocketError(error) {
+          console.log('Error: ' + error.data);
+      };
+
+      // Socket connected
+      function onSocketConnected() {
+          console.log("Connected to socket server");
+      };
+
+      // Socket disconnected
+      function onSocketDisconnect() {
+          console.log("Disconnected from socket server");
+      };
+
+   //  ws.onopen = function()
+   // {
+   //    ws.send("Message to send");
+   //    alert("package sent")
+   //  };
+
+   //  ws.onmessage = function (evt)
+   //  {
+   //    var received_msg = evt.data;
+   //    alert("package is received...");
+   //  };
+
+   //  ws.onclose = function()
+   //  {
+   //    // websocket is closed.
+   //    alert("Connection is closed...");
+   //  };
+  }
+
 }
