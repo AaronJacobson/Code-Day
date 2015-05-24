1856f0650e6847a06a80e585883e65492ab0349f
Ian Flaherty <ianrocket@gmail.com>
2015-05-23 18:18:07 -0700

    basic build of board in js

--- main.js
+++ main.js
@@ -1,40 +1,82 @@
-var url,
-  protocal,
-  ws;
-
-
 var draw = function(){
-  var canvas = new Canvas();
+
+  //window is 601,750
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+
+  ctx.fillRect(99,174,403,403);
+
+  ctx.beginPath();
+  ctx.moveTo(199,174);
+  ctx.lineTo(199,577);
+  ctx.moveTo(299,174);
+  ctx.lineTo(299,577);
+  ctx.moveTo(399,174);
+  ctx.lineTo(399,577);
+
+
+  ctf.fillStyle("#00008F");
+  ctx.arc(150,100,100,0,Math.PI*2,true);
+  ctf.fill();
+  ctx.arc(300,100,100,0,Math.PI*2,true);
+  ctf.fill();
+  ctx.arc(450,100,100,0,Math.PI*2,true);
+  ctf.fill();
+
+  //#A30000 p2 red
+  ctf.fillStyle("#A30000");
+  ctx.arc(150,650,100,0,Math.PI*2,true);
+  ctf.fill();
+  ctx.arc(300,650,100,0,Math.PI*2,true);
+  ctf.fill();
+  ctx.arc(450,650,100,0,Math.PI*2,true);
+  ctf.fill();
+
+
+  //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+  //ctx.arc(225,150,50,0,Math.PI*2,true);
+
+  // for(var x= 0; x<= 3; x++){
+  //   for(var y =0 ; y<= 3; y++){
+  //   }
+  // }
+
 }
 
-function WebSocketTest()
-{
-  if("webSocket" in window){
-    ws = new webSocket(url, protocal);
-    ws.onopen = onSocketConnected();
-    ws.onclose = onSocketDisconnect();
-    ws.onmessage = onSocketMessage();
-    ws.onerror = onSocketError();
+// function WebSocketTest()
+// {
+//   if("webSocket" in window){
+//     url = "a url"
+//     protocal = "tcp"
+//     ws = new webSocket(url, protocal);
+//     ws.onopen = onSocketConnected();
+//     ws.onclose = onSocketDisconnect();
+//     ws.onmessage = onSocketMessage();
+//     ws.onerror = onSocketError();
 
-       // Socket message
-      function onSocketMessage(message) {
-          console.log('Message: ' + message.data);
-      };
 
-      // Socket error
-      function onSocketError(error) {
-          console.log('Error: ' + error.data);
-      };
+//     ws.send("hi i work");
 
-      // Socket connected
-      function onSocketConnected() {
-          console.log("Connected to socket server");
-      };
 
-      // Socket disconnected
-      function onSocketDisconnect() {
-          console.log("Disconnected from socket server");
-      };
+//        // Socket message
+//       function onSocketMessage(message) {
+//           console.log('Message: ' + message.data);
+//       };
+
+//       // Socket error
+//       function onSocketError(error) {
+//           console.log('Error: ' + error.data);
+//       };
+
+//       // Socket connected
+//       function onSocketConnected() {
+//           console.log("Connected to socket server");
+//       };
+
+//       // Socket disconnected
+//       function onSocketDisconnect() {
+//           console.log("Disconnected from socket server");
+//       };
 
    //  ws.onopen = function()
    // {
@@ -53,6 +95,6 @@ function WebSocketTest()
    //    // websocket is closed.
    //    alert("Connection is closed...");
    //  };
-  }
+  // }
 
-}
+//}
